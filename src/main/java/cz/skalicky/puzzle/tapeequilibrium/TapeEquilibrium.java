633c54385a5540d7608c7ed01fccc39a2f7fc468
Tomas Skalicky <skalicky.tomas@gmail.com>
2015-09-25 19:50:03 +0200

    initial commit with tape-equilibrium and two-rectangle-intersection

---
+++ src/main/java/cz/skalicky/puzzle/tapeequilibrium/TapeEquilibrium.java
@@ -0,0 +1,72 @@
+package cz.skalicky.puzzle.tapeequilibrium;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+import org.springframework.util.Assert;
+
+/**
+ * @see https://codility.com/programmers/task/tape_equilibrium
+ * @author Tomas Skalicky
+ */
+public class TapeEquilibrium {
+
+    public static void main(String[] args) {
+
+        runTest(new int[] { 1 }, -1);
+        runTest(new int[] { 1, 2 }, 1);
+        runTest(new int[] { 1, 2, 3 }, 0);
+        runTest(new int[] { 1, 0, -1 }, 2);
+        runTest(new int[] { 3, 1, 2, 4, 3 }, 1);
+        System.out.println("SUCCESS");
+    }
+
+    private static void runTest(final int[] tape, final int expectedMinDifference) {
+
+        Assert.isTrue(getMinDifference(tape) == expectedMinDifference, String.format(
+                "Min difference in tape %s should be %d.", Arrays.toString(tape), expectedMinDifference));
+    }
+
+    private static class CalculationState {
+
+        private int leftPartSum;
+        private int rightPartSum;
+
+        private CalculationState(final int leftPartSum, final int rightPartSum) {
+            this.leftPartSum = leftPartSum;
+            this.rightPartSum = rightPartSum;
+        }
+
+        private int getDifference() {
+            return Math.abs(leftPartSum - rightPartSum);
+        }
+
+        private void moveRight(final int number) {
+            leftPartSum += number;
+            rightPartSum -= number;
+        }
+
+        private int moveRightAndGetDifference(final int number) {
+            moveRight(number);
+            return getDifference();
+        }
+    }
+
+    private static int getMinDifference(final int[] tape) {
+
+        if (tape.length < 2) {
+            return -1;
+        }
+
+        final int tapeSum = Arrays.stream(tape).sum();
+        final CalculationState state = new CalculationState(0, tapeSum);
+        // @formatter:off
+        final int minDifference =
+                IntStream.range(0, tape.length - 1)
+                .map(i -> state.moveRightAndGetDifference(tape[i]))
+                .min().getAsInt();
+        // @formatter:on
+        return minDifference;
+    }
+
+}
---
+++ src/main/java/cz/skalicky/puzzle/tworectanglesintersetion/Solution.java
@@ -0,0 +1,58 @@
+package cz.skalicky.puzzle.tworectanglesintersetion;
+
+import org.springframework.util.Assert;
+
+public class Solution {
+
+    private static class Point {
+        private final int x;
+        private final int y;
+
+        private Point(final int x, final int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        final Solution solution = new Solution();
+
+        Assert.isTrue(solution.solution(0, 2, 5, 10, 3, 1, 20, 15) == 16);
+        Assert.isTrue(solution.solution(0, 2, 5, 10, 5, 1, 20, 15) == 0);
+        Assert.isTrue(solution.solution(0, 2, 5, 10, 5, 10, 20, 15) == 0);
+        Assert.isTrue(solution.solution(0, 2, 5, Integer.MAX_VALUE / 2, 2, 10, 20, Integer.MAX_VALUE) == -1);
+        System.out.println("SUCCESS");
+    }
+
+    private int solution(final int k, final int l, final int m, final int n, final int p, final int q,
+            final int r, final int s) {
+        final Point rectangle1LowerLeft = new Point(k, l);
+        final Point rectangle1UpperRight = new Point(m, n);
+        final Point rectangle2LowerLeft = new Point(p, q);
+        final Point restangle2UpperRight = new Point(r, s);
+
+        final int lowerLeftX = Math.max(rectangle1LowerLeft.x, rectangle2LowerLeft.x);
+        final int lowerLeftY = Math.max(rectangle1LowerLeft.y, rectangle2LowerLeft.y);
+        final Point intersectionLowerLeft = new Point(lowerLeftX, lowerLeftY);
+
+        final int upperRightX = Math.min(rectangle1UpperRight.x, restangle2UpperRight.x);
+        final int upperRightY = Math.min(rectangle1UpperRight.y, restangle2UpperRight.y);
+        final Point intersectionUpperRight = new Point(upperRightX, upperRightY);
+
+        if ((intersectionLowerLeft.x >= intersectionUpperRight.x)
+                || (intersectionLowerLeft.y >= intersectionUpperRight.y)) {
+            return 0;
+        } else {
+            final int length = intersectionUpperRight.x - intersectionLowerLeft.x;
+            final int high = intersectionUpperRight.y - intersectionLowerLeft.y;
+            final long product = length * (long) high;
+            if (product > Integer.MAX_VALUE) {
+                return -1;
+            } else {
+                return (int) product;
+            }
+        }
+    }
+
+}
